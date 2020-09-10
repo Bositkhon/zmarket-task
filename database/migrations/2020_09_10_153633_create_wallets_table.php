03957cc840df08768586570ac39b6582a033b5ab
Bositkhon <bositkhon.sh@gmail.com>
2020-09-10 22:48:01 +0500

    fix: drop foreign key

--- database/migrations/2020_09_10_153633_create_wallets_table.php
+++ database/migrations/2020_09_10_153633_create_wallets_table.php
@@ -30,6 +30,9 @@ class CreateWalletsTable extends Migration
      */
     public function down()
     {
+        Schema::table('wallets', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
         Schema::dropIfExists('wallets');
     }
 }
