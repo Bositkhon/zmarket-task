e6f019b70c98c1680c325cb24e9b5e6a20a95590
Bositkhon <bositkhon.sh@gmail.com>
2020-09-10 22:27:12 +0500

    feat: wallet migration and model created

---
+++ app/Models/Wallet.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Wallet extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * Mass assignable properties
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'balance'
+    ];
+
+    /**
+     * A user model that wallet belongs to
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+}
